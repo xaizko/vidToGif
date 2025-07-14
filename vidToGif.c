6b68cc2b1e3bcae3fc24b06292daf502a43006ff
xaizko <tonytran177013@gmail.com>
2025-07-14 15:50:53 -0400

    added getting format context

--- vidToGif.c
+++ vidToGif.c
@@ -7,4 +7,14 @@ int main(int argc, char *argv[]) {
 	fprintf(stderr, "Usage: %s <path to video> <desired output path>\n", argv[0]);
 	return -1;
     }
+
+    const char *input_path = argv[1];
+    const char *output_path = argv[2];
+
+    //get format context
+    AVFormatContext *format_ctx = NULL;
+    if (avformat_open_input(&format_ctx, input_path, NULL, NULL) != 0) {
+	fprintf(stderr, "Could not open input file (%s)\n", input_path);
+	return -1;
+    }
 }
