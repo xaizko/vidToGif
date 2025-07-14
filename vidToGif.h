ce2a4003a7c45a6ea813a8d2c4c67f866248ca07
xaizko <tonytran177013@gmail.com>
2025-07-14 10:00:17 -0400

    added proper libraries

---
+++ vidToGif.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//FFmpeg libraries
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
