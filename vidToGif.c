3011c8e1ecb3ecc3ece6936b4122eb68dd8588ed
xaizko <tonytran177013@gmail.com>
2025-07-23 20:41:59 -0400

    added actual usage of file ccheck

--- vidToGif.c
+++ vidToGif.c
@@ -4,6 +4,10 @@ int main(int argc, char *argv[]) {
     if (argc < 3) {
 	printf("Usage: %s <path/to/input> <path/to/output/\n", argv[0]);
     }
+
+    if (!is_supported_file(argv[1])) {
+	printf("Unsupported file type: %s\nPlease input a .mov or .mp4\n", argv[1]);
+    }
 }
 
 bool is_supported_file(const char *filename) {
