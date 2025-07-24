e6994064e130d905aa3fc4cf465eccc2bf9d067b
xaizko <tonytran177013@gmail.com>
2025-07-23 21:06:09 -0400

    Added output check

--- vidToGif.c
+++ vidToGif.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]) {
     if (!is_supported_file(argv[1])) {
 	printf("Unsupported file type: %s\nPlease input a .mov or .mp4\n", argv[1]);
     }
+
+    if (!is_supported_output(argv[2])) {
+	printf("Unsupported output type: %s\n", argv[2]);
+	printf("Output must end with .gif\n");
+    }
 }
 
 bool is_supported_file(const char *filename) {
@@ -15,4 +20,11 @@ bool is_supported_file(const char *filename) {
     if (!dot) return false;
 
     return strcmp(dot, ".mp4") == 0 || strcmp(dot, ".mov") == 0;
+}
+
+bool is_supported_output(const char *output) {
+    const char *dot = strrchr(output, '.');
+    if (!dot) return false;
+
+    return strcmp(dot, ".gif") == 0;
 }
--- vidToGif.h
+++ vidToGif.h
@@ -2,4 +2,6 @@
 #include <string.h>
 #include <stdbool.h>
 
+//file check functions 
 bool is_supported_file(const char *filename);
+bool is_supported_output(const char *output);
